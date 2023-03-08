b90b1015df4880e09ebfd17c4cb53ae1d73d5c62
Lvajtner <vajtner@ualberta.ca>
2023-03-07 17:41:13 -0700

    Update CustomListTest.java

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -8,9 +8,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private ArrayList<City> citylist = new ArrayList<>();;
 
+    @Test
+    public void addCityTest(){
+        CustomList list = new CustomList(null, citylist);
+        int listSize = list.getCount();
+        City city = new City("Edmonton", "AB");
+        list.addCity(city);
+        assertEquals(list.getCount(), listSize +1);
+    }
 
 }
